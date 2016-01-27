0c7d366a93ce50d673240851b21a867e29b60a36
anna becker and matthew newell <anna.r.becker@gmail.com>
2016-01-27 13:57:44 -0800

    Refactor specs and scrpits with methods and constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
-function randomWord() {
+function Hangman(userInput) {
+  this.userInput = userInput
+}
+
+Hangman.prototype.randomWord = function() {
   var words = ["cat", "harse", "pag"];
   var rand = words[Math.floor(Math.random() * words.length)];
   return rand;
 }
 
-function replaceBlanks() {
+Hangman.prototype.replaceBlanks = function() {
   var array = [];
-  var rand = randomWord();
+  var rand = this.randomWord();
   for(var i=0; i < rand.length; i++){
       array.push("-");
   }
 return array;
 }
 
-function replaceLetters () {
+Hangman.prototype.replaceLetters = function() {
   var userInput = "a"; // this obviously isnt user input
   var wordToBeReturned = "";
   //replace blanks when the user correcly enters a letter
@@ -30,13 +34,3 @@ function replaceLetters () {
   }
   return wordToBeReturned;
 }
-
-
-
-
-
-// the user is putting in a letter:
-// that letter is compared to the randomly selected word:
-// the random word needs to be split into an array full of ["c", "a", "t"]:
-// if the user input("LETTER") ===  "c" || "a" || "t"
-// then return that letter and all other instances of that letter
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,35 @@
-describe('randomWord', function() {
-  it("select a random word from an array", function () {
-  expect(randomWord("cat")).to.be.a('string');
-  });
-});
+// describe('randomWord', function() {
+  // it("select a random word from an array", function () {
+//   expect(randomWord("cat")).to.be.a('string');
+//   });
+// });
+//
+// describe('replaceBlanks', function() {
+//   it("replace the letters in the word with one blank for each letter", function() {
+//     expect(replaceBlanks("cat")).to.eql(["-", "-", "-"]);
+//   });
+// });
+//
+// describe('replaceLetters', function() {
+//   it("replace the blanks when the user correcly enters a letter", function() {
+//     expect(replaceLetters("a")).to.equal("-a-");
+//   });
+// });
 
-describe('replaceBlanks', function() {
-  it("replace the letters in the word with one blank for each letter", function() {
-    expect(replaceBlanks("cat")).to.eql(["-", "-", "-"]);
-  });
-});
 
-describe('replaceLetters', function() {
-  it("replace the blanks when the user correcly enters a letter", function() {
-    expect(replaceLetters("a")).to.equal("-a-");
+describe('Hangman', function() {
+  it("selects a random word from an array", function() {
+    var testHangman = new Hangman();
+    expect(testHangman.randomWord("cat")).to.be.a('string');
+  });
+
+  it("replaces the letters in the word with one blank for each letter", function() {
+    var testHangman = new Hangman();
+    expect(testHangman.replaceBlanks("cat")).to.eql(["-", "-", "-"]);
+  });
+
+  it("replaces the blanks when the user correcly enters a letter", function() {
+    var testHangman = new Hangman();
+    expect(testHangman.replaceLetters("a")).to.equal("-a-");
   });
 });
